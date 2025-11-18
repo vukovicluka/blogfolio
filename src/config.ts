2e46c0ec13c9c78be27d44e7bbd574ff59e4e0f7
Luka Vukovic <vukovicluka227@gmail.com>
2025-11-18 14:37:50 +0100

    Move to remote

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const SOCIAL_LINKS = {
+  email: "vukovicluka227@gmail.com",
+  linkedin: "https://www.linkedin.com/in/luka-vukovic/",
+  github: "https://github.com/vukovicluka",
+} as const;
