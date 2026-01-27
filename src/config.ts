be9084a2b80cd16a8a94ca3afeceb795c6ba293c
Luka Vukovic <vukovicluka227@gmail.com>
2026-01-27 10:48:02 +0100

    change email address

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SOCIAL_LINKS = {
-  email: "vukovicluka227@gmail.com",
+  email: "lukvuk@proton.me",
   linkedin: "https://www.linkedin.com/in/luka-vukovic/",
   github: "https://github.com/vukovicluka",
 } as const;
