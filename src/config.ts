b822078f6e7d85c3042fc7a9d5de20ee5a57dd6a
Luka Vukovic <vukovicluka227@gmail.com>
2026-01-27 15:35:56 +0100

    add stackoverflow account link to the footer

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export const SOCIAL_LINKS = {
   email: "lukvuk@proton.me",
   linkedin: "https://www.linkedin.com/in/luka-vukovic/",
   github: "https://github.com/vukovicluka",
+  stackoverflow: "https://stackoverflow.com/users/16994268/vukovicluka",
 } as const;
